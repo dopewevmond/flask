fe5eab11ae72dbea576779ca1026cfab6702f140
Edmund Wevamwo <dopewevmond@gmail.com>
2021-07-01 16:32:39 +0000

    using templates I

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,9 @@
+from flask import render_template
 from app import app
 
+
 @app.route('/')
-@app.route('/index5')
+@app.route('/index')
 def index():
-    return "Hello world"
+    user = {'username': 'Edmund'}
+    return render_template('index.html', title='Home', user=user)
