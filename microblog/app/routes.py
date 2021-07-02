84a496f0d8e50041595857e5d670a5842253c4f5
Edmund Wevamwo <dopewevmond@gmail.com>
2021-07-02 22:46:11 +0000

    conditionals and loops in templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Edmund'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
